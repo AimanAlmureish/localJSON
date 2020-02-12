3e6e9bf0054d87bff87d23826dbf4067d69557ee
AimanAlmureish <aiman.almureish@gmail.com>
2020-02-12 20:50:32 +0300

    fixing up json-server

--- script.js
+++ script.js
@@ -1,42 +1,74 @@
-// let h1 = document.querySelector("h1");
-let div = document.querySelector("div");
-// https://jsonplaceholder.typicode.com/posts
-fetch("./db.json")
-  .then(resp => resp.json())
-  .then(data => {
-    console.log(data);
-    // data.map(item => {
-    //   console.log(item.id);
-    //   h1.innerHTML = item.id;
-    // });
-    data.map(element => {
-      let h1 = document.createElement("h1");
-      h1.innerHTML = element.id;
-      div.appendChild(h1);
-    });
-  });
+// // let h1 = document.querySelector("h1");
+// let div = document.querySelector("div");
+// // https://jsonplaceholder.typicode.com/posts
+// fetch("./db.json")
+//   .then(resp => resp.json())
+//   .then(data => {
+//     console.log(data);
+//     // data.map(item => {
+//     //   console.log(item.id);
+//     //   h1.innerHTML = item.id;
+//     // });
+//     data.map(element => {
+//       let h1 = document.createElement("h1");
+//       h1.innerHTML = element.id;
+//       div.appendChild(h1);
+//     });
+//   });
 
-let id = document.querySelector("input");
+// let id = document.querySelector("input");
+// let button = document.querySelector("button");
+
+// button.onclick = function(e) {
+//   console.log("object");
+//   console.log(id.value);
+
+//   fetch("./db.json", {
+//     method: "POST",
+//     headers: {
+//       "Access-Control-Allow-Origin": "*",
+//       Allow: "POST",
+//       "Content-type": "application/json",
+//       Accept: "application/json"
+//     },
+//     body: JSON.stringify({
+//       id: id.value
+//     })
+//   })
+//     .then(resp => resp.json())
+//     .then(data => {
+//       console.log(data);
+//     });
+//   e.preventDefault();
+// };
+
+const getIds = async () => {
+  const resp = await fetch("http://localhost:3000/ids");
+  const data = await resp.json();
+  console.log(data);
+};
+
+getIds();
+
 let button = document.querySelector("button");
 
-button.onclick = function(e) {
-  e.preventDefault();
-  console.log("object");
-  console.log(id.value);
-
-  fetch("./db.json", {
-    method: "POST",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify({
-      id: id.value
-    })
-  })
-    .then(resp => resp.json())
-    .then(data => {
-      console.log(data);
+button.onclick = () => {
+  const postIds = async () => {
+    let id = document.querySelector("input");
+    const resp = await fetch("http://localhost:3000/ids", {
+      method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        mode: "no-cors",
+        "Content-Type": "application/json;charset=utf-8"
+      },
+      body: JSON.stringify({
+        id: parseInt(id.value)
+      })
     });
+    const data = await resp.json();
+    return data;
+  };
+
+  postIds().then(data => console.log(data));
 };
